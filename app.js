1027911aa8e04c80d76d9e53e74d441f14fc5ec5
venkat-2608 <sudheendra.mv@gmail.com>
2026-04-16 00:04:08 +0530

    Added validation and dynamic message handling in AngularJS controller

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ var app = angular.module("registerApp", []);
 app.controller("registerController", function($scope){
 
 $scope.user = {};
+$scope.message = "";
 
 $scope.submitForm = function(){
 
-$scope.message = "Registration Successful! Welcome " + $scope.user.name;
+if($scope.user.name && $scope.user.email){
+    $scope.message = "Registration Successful! Welcome " + $scope.user.name;
+} else {
+    $scope.message = "Please fill all required fields";
+}
 
 };
 
