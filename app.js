7df4c445eefb24c6880ceb1e667763edcedef397
venkat-2608 <sudheendra.mv@gmail.com>
2026-04-16 00:26:22 +0530

    Added state management and reset functionality with submitted user tracking

--- app.js
+++ app.js
@@ -3,16 +3,30 @@ var app = angular.module("registerApp", []);
 app.controller("registerController", function ($scope) {
 
     $scope.user = {};
+    $scope.submittedUser = null;
     $scope.message = "";
 
     $scope.submitForm = function (form) {
 
         if (form.$valid) {
-            $scope.message = "Registration Successful! Welcome " + $scope.user.name;
+            $scope.submittedUser = angular.copy($scope.user);
+            $scope.message = "Registration Successful!";
+
+            // Reset input fields
+            $scope.user = {};
+            form.$setPristine();
+            form.$setUntouched();
         } else {
             $scope.message = "Please fix the errors before submitting";
         }
 
+    };
+
+    $scope.resetForm = function (form) {
+        $scope.user = {};
+        $scope.message = "";
+        form.$setPristine();
+        form.$setUntouched();
     };
 
 });
