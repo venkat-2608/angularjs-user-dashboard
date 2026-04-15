322ee6b27e4ade935a021b5619ee6d31db880e89
venkat-2608 <sudheendra.mv@gmail.com>
2026-04-16 00:18:48 +0530

    Implemented AngularJS form validation using built-in form states

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 var app = angular.module("registerApp", []);
 
-app.controller("registerController", function($scope){
+app.controller("registerController", function ($scope) {
 
-$scope.user = {};
-$scope.message = "";
+    $scope.user = {};
+    $scope.message = "";
 
-$scope.submitForm = function(){
+    $scope.submitForm = function (form) {
 
-if($scope.user.name && $scope.user.email){
-    $scope.message = "Registration Successful! Welcome " + $scope.user.name;
-} else {
-    $scope.message = "Please fill all required fields";
-}
+        if (form.$valid) {
+            $scope.message = "Registration Successful! Welcome " + $scope.user.name;
+        } else {
+            $scope.message = "Please fix the errors before submitting";
+        }
 
-};
+    };
 
 });
